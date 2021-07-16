75751f7a82601a840acf021aa9e8e77c4dda1ae8
yangliang <mac@123456.com>
2021-07-16 22:52:52 +0800

    tls+token 认证

--- client/client.go
+++ client/client.go
@@ -12,19 +12,52 @@ import (
 	"google.golang.org/grpc/grpclog"
 )
 
-var (
-	Address = viper.GetString("GRPC_ADDR")
+const (
+	SERVER_COMMON_NAME = "www.hello.com"
 )
 
+var (
+	Address = viper.GetString("GRPC_ADDR")
+	OpenTLS = viper.GetBool("OpenTLS")
+	appid   = viper.GetString("appid")
+	appkey  = viper.GetString("appkey")
+)
+
+type customCredentials struct{}
+
+// 自定义认证接口
+func (customCredentials) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"appid":  appid,
+		"appkey": appkey,
+	}, nil
+}
+
+// 自定义是否开启 tls
+func (customCredentials) RequireTransportSecurity() bool {
+	return OpenTLS
+}
+
 func main() {
-	// tls 认证
-	creads, err := credentials.NewClientTLSFromFile("../keys/server.pem", "go-grpc-example")
-	if err != nil {
-		grpclog.Fatalf("Failed to generate TLS credentials %v", err)
-		return
+
+	var err error
+	var opts []grpc.DialOption
+
+	if OpenTLS {
+		// tls 认证
+		creads, err := credentials.NewClientTLSFromFile("../keys/server.pem", SERVER_COMMON_NAME)
+		if err != nil {
+			grpclog.Fatalf("Failed to generate TLS credentials %v", err)
+			return
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creads))
+	} else {
+		opts = append(opts, grpc.WithInsecure())
 	}
+	opts = append(opts, grpc.WithPerRPCCredentials(new(customCredentials)))
+
 	// 连接
-	conn, err := grpc.Dial(Address, grpc.WithTransportCredentials(creads))
+	conn, err := grpc.Dial(Address, opts...)
 	if err != nil {
 		grpclog.Fatalln("server dial error: ", err)
 		return
@@ -35,7 +68,7 @@ func main() {
 	// 初始化客户端
 	c := pb.NewHelloClient(conn)
 	// 调用方法
-	query := &pb.HelloRequest{Name: "wangwu"}
+	query := &pb.HelloRequest{Name: "yangliang"}
 	res, err := c.SayHello(context.Background(), query)
 
 	if err != nil {
--- server/server.go
+++ server/server.go
@@ -7,8 +7,11 @@ import (
 	"net"
 
 	"github.com/spf13/viper"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 
 	"google.golang.org/grpc"
 
@@ -23,9 +26,27 @@ type helloService struct{}
 
 var (
 	HelloService = helloService{}
+	appid        string
+	appkey       string
 )
 
 func (helloService) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Errorf(codes.Unauthenticated, "无Token认证信息")
+	}
+	if val, ok := md["appid"]; ok {
+		appid = val[0]
+	}
+	if val, ok := md["appkey"]; ok {
+		appkey = val[0]
+	}
+
+	if appid != viper.GetString("appid") || appkey != viper.GetString("appkey") {
+		return nil, status.Errorf(codes.Unauthenticated, "Token认证信息无效: appid=%s, appkey=%s", appid, appkey)
+	}
+
 	resp := new(pb.HelloResponse)
 	resp.Message = fmt.Sprintf("Hello %s \n ", in.Name)
 
@@ -49,7 +70,7 @@ func main() {
 	s := grpc.NewServer(grpc.Creds(creads))
 	// 注册服务
 	pb.RegisterHelloServer(s, HelloService)
-	fmt.Printf("Listen on %s with TLS", Address)
+	fmt.Printf("Listen on %s with TLS and Token", Address)
 	// 启动服务
-	err = s.Serve(listen)
+	s.Serve(listen)
 }
